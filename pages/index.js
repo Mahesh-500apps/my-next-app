8c8a46d2b5e3b2f535f67fe07cf8409ad5a7c09b
mahesh-500apps <mahesh.atla@mantragroup.com>
2024-11-21 14:58:45 +0530

    my new app

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js Application!</h1>
+      <p>This is a simple page created using Next.js.</p>
+    </div>
+  );
+}
